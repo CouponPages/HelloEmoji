93f5aafc1dbae3242abc31608715ba75959e783b
Joe Crescenzi <CouponPages@gmail.com>
2017-08-31 06:28:45 -0400

    TableViewController is empty, but works.

---
+++ Emoji Dictionary/EmojiTableViewController.swift
@@ -0,0 +1,37 @@
+//
+//  EmojiTableViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Joe Crescenzi on 8/31/17.
+//  Copyright © 2017 Joe Crescenzi. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+}
