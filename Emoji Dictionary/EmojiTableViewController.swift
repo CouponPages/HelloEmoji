2ccd06f8812f83cde491a9c0436a1c0f1e494684
Joe Crescenzi <CouponPages@gmail.com>
2017-08-31 07:00:13 -0400

    Working navigation sections.

--- Emoji Dictionary/EmojiTableViewController.swift
+++ Emoji Dictionary/EmojiTableViewController.swift
@@ -27,6 +27,10 @@ class EmojiTableViewController: UITableViewController {
         return Emojis.count
     }
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Tapped!")
+        performSegue(withIdentifier: "MySeque", sender: nil)
+    }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
